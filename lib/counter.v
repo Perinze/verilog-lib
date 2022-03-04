3fde1d13d340b362eb67919f3491e356deb51d2a
perinze <perinzefaper@foxmail.com>
2022-03-04 08:30:28 +0800

    counter

--- lib/counter.v
+++ lib/counter.v
@@ -1,25 +1,16 @@
 module counter # (
-<<<<<<< HEAD
-    parameter WIDTH = 16
-) (
-    input clk,
-    input clear,
-    output reg []
-)
-=======
 	parameter WIDTH = 16
 ) (
 	input clk,
-	input rstn,
+	input clear,
 	output reg [WIDTH-1:0] cnt
 );
 
 always @(posedge clk) begin
-	if (rstn == 1'b0)
+	if (~clear)
 		cnt <= {WIDTH{1'b0}};
 	else
 		cnt <= cnt + 1;
 end
 
 endmodule
->>>>>>> 7f2114518f79db44e927c1c2991b3c13fea81e5e
