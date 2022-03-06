49792d9e0c2316c66cce2ed1789e07cf1a7915c5
perinze <perinzefaper@foxmail.com>
2022-03-06 17:20:23 +0800

    fsm done

--- wash_fsm.v
+++ wash_fsm.v
@@ -1,5 +1,7 @@
 module wash_fsm # (
-    parameter WIDTH = 5,
+    parameter CNTW = 5,      // count width
+    parameter SW = 3,       // state width
+    parameter MW = 2,       // motor width
     parameter CW = 4,
     parameter CCW = 4,
     parameter PAUSE = 2,
@@ -14,81 +16,47 @@ module wash_fsm # (
 ) (
     input clk,		// 1Hz
     input rst_n,
-    input start,
     // 0 for stop, 1 for clockwise, 2 for counterclockwise, 3 undefined
     output [1:0] motor,
     // 0 for stopping, 1 for working
     // negedge indicate completion
-    output reg compl_n
+    output compl_n
 );
 
-wire [WIDTH-1:0] cnt;
-reg [2:0] state;
-wire [2:0] next;
+wire [CNTW-1:0] cnt;
+reg [SW-1:0] state;
+wire [SW-1:0] next;
 wire clear;
 reg clear_reg;
 
-counter #(.WIDTH(WIDTH))    c0	(	.clk(clk),
+counter #(.WIDTH(CNTW))    c0	(	.clk(clk),
 			                        .rst_n(clear),
 			                        .cnt(cnt));
 
-assign motor = (state == S1 ? M1 :
-                state == S2 ? M0 :
-                state == S3 ? M2 :
-                state == S4 ? M0 : M0);
+assign motor = {SW{rst_n}} &   (state == S1 ? M1 :
+                                state == S2 ? M0 :
+                                state == S3 ? M2 :
+                                state == S4 ? M0 : M0);
 
-/*
-assign compl_n = (  state == S1 ? 1 :
-                    state == S2 ? 1 :
-                    state == S3 ? 1 :
-                    state == S4 ? 1 : 0);
-                    */
-//assign compl_n = (state[0] | state[1] | state[2]);
-
-assign clear = rst_n | clear_reg;
+assign clear = rst_n & over;
 
 assign next =  (state == S1 && cnt == CW    ? S2 :
                 state == S2 && cnt == PAUSE ? S3 :
                 state == S3 && cnt == CCW   ? S4 :
                 state == S4 && cnt == PAUSE ? S0 : state);
 
-always@(posedge clk) begin
-    if (~rst_n) begin
-        state <= S0;
-        compl_n <= 0;
-        clear_reg <= 0;
-    end
-    case(state)
-        S1: begin
-            state <= (cnt == CW ? S2 : state);
-            clear_reg <= (cnt == CW ? 1 : 0);
-            compl_n <= 1;
-        end
-        S2: begin
-            state <= (cnt == PAUSE ? S3 : state);
-            clear_reg <= (cnt == PAUSE ? 1 : 0);
-            compl_n <= 1;
-        end
-        S3: begin
-            state <= (cnt == CCW ? S4 : state);
-            clear_reg <= (cnt == CCW ? 1 : 0);
-            compl_n <= 1;
-        end
-        S4: begin
-            state <= (cnt == PAUSE ? S0 : state);
-            clear_reg <= (cnt == PAUSE ? 1 : 0);
-            compl_n <= 1;
-        end
-        default: begin
-            state <= S0;
-            clear_reg <= 0;
-            compl_n <= 0;
-        end
-    endcase
-end
+assign over =  (state == S1 && cnt == CW    ? 0 :
+                state == S2 && cnt == PAUSE ? 0 :
+                state == S3 && cnt == CCW   ? 0 :
+                state == S4 && cnt == PAUSE ? 0 : 1);
 
-always@(posedge rst_n) begin
-    state <= S1;
+assign compl_n = (state != S0);
+
+always@(posedge clk) begin
+    if (~rst_n)
+        state <= S1;
+    else
+        state <= next;
 end
 
 endmodule
--- wash_fsm_tb.v
+++ wash_fsm_tb.v
@@ -1,19 +1,17 @@
-`timescale 1ns/1ps
+`timescale 1s/1us
 
 module wash_fsm_tb;
     parameter WIDTH     = 16;
-    parameter FREQ      = 40_000;
-    parameter PERIOD    = 10_000_000_000;
+    parameter FREQ      = 1;
+    parameter PERIOD    = 30;
 
     reg     clk;    // 40kHz
     reg     rst_n;
-	reg 	start;
 	wire 	[1:0]	motor;
 	wire 	compl_n;
 
     wash_fsm c0 (	.clk(clk),
 		 		   	.rst_n(rst_n),
-					.start(start),
 					.motor(motor),
 					.compl_n(compl_n));
 
@@ -23,19 +21,17 @@ module wash_fsm_tb;
         $dumpvars(0, wash_fsm_tb);
     end
 
-    always #(500_000_000 / FREQ)
+    always #(0.5 / FREQ)
         clk     <= ~clk;
 
     initial begin
-        clk		<= 1'b0;
-        rst_n	<= 1'b0;
-		start	<= 1'b0;
+        clk		<= 1'b1;
 
-        //#(1_000_000_000 / FREQ) rst_n	<= 1'b1;
-        #(50_000) rst_n	<= 1'b1;
-        //#(75_000) rst_n	<= 1'b0;
-		//#(50_000)				start	<= 1'b1;
-		//#(75_000)				start	<= 1'b0;
+        rst_n	<= 1'b0;
+        #(2) rst_n	<= 1'b1;
+
+        #(15) rst_n <= 1'b0;
+        #(16) rst_n <= 1'b1;
 
         #PERIOD $finish;
     end
