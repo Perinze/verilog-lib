373361131c0bbc62effd02411fd041e0d3b46132
perinze <perinzefaper@foxmail.com>
2022-03-06 15:55:04 +0800

    ready for use next in fsm

--- wash_fsm.v
+++ wash_fsm.v
@@ -1,8 +1,8 @@
 module wash_fsm # (
     parameter WIDTH = 5,
-    parameter CW = 20,
-    parameter CCW = 20,
-    parameter PAUSE = 10,
+    parameter CW = 4,
+    parameter CCW = 4,
+    parameter PAUSE = 2,
     parameter S0 = 0,
     parameter S1 = 1,
     parameter S2 = 2,
@@ -19,11 +19,12 @@ module wash_fsm # (
     output [1:0] motor,
     // 0 for stopping, 1 for working
     // negedge indicate completion
-    output compl_n
+    output reg compl_n
 );
 
 wire [WIDTH-1:0] cnt;
-reg [1:0] state;
+reg [2:0] state;
+wire [2:0] next;
 wire clear;
 reg clear_reg;
 
@@ -36,12 +37,20 @@ assign motor = (state == S1 ? M1 :
                 state == S3 ? M2 :
                 state == S4 ? M0 : M0);
 
+/*
 assign compl_n = (  state == S1 ? 1 :
                     state == S2 ? 1 :
                     state == S3 ? 1 :
                     state == S4 ? 1 : 0);
+                    */
+//assign compl_n = (state[0] | state[1] | state[2]);
 
 assign clear = rst_n | clear_reg;
+
+assign next =  (state == S1 && cnt == CW    ? S2 :
+                state == S2 && cnt == PAUSE ? S3 :
+                state == S3 && cnt == CCW   ? S4 :
+                state == S4 && cnt == PAUSE ? S0 : state);
 
 always@(posedge clk) begin
     if (~rst_n) begin
@@ -53,24 +62,33 @@ always@(posedge clk) begin
         S1: begin
             state <= (cnt == CW ? S2 : state);
             clear_reg <= (cnt == CW ? 1 : 0);
+            compl_n <= 1;
         end
         S2: begin
             state <= (cnt == PAUSE ? S3 : state);
             clear_reg <= (cnt == PAUSE ? 1 : 0);
+            compl_n <= 1;
         end
         S3: begin
             state <= (cnt == CCW ? S4 : state);
             clear_reg <= (cnt == CCW ? 1 : 0);
+            compl_n <= 1;
         end
         S4: begin
             state <= (cnt == PAUSE ? S0 : state);
             clear_reg <= (cnt == PAUSE ? 1 : 0);
+            compl_n <= 1;
         end
         default: begin
             state <= S0;
             clear_reg <= 0;
+            compl_n <= 0;
         end
     endcase
+end
+
+always@(posedge rst_n) begin
+    state <= S1;
 end
 
 endmodule
--- wash_fsm_tb.v
+++ wash_fsm_tb.v
@@ -3,7 +3,7 @@
 module wash_fsm_tb;
     parameter WIDTH     = 16;
     parameter FREQ      = 40_000;
-    parameter PERIOD    = 4_000_000_000;
+    parameter PERIOD    = 10_000_000_000;
 
     reg     clk;    // 40kHz
     reg     rst_n;
@@ -31,9 +31,11 @@ module wash_fsm_tb;
         rst_n	<= 1'b0;
 		start	<= 1'b0;
 
-        #(1_000_000_000 / FREQ) rst_n	<= 1'b1;
-		#(50_000)				start	<= 1'b1;
-		#(75_000)				start	<= 1'b0;
+        //#(1_000_000_000 / FREQ) rst_n	<= 1'b1;
+        #(50_000) rst_n	<= 1'b1;
+        //#(75_000) rst_n	<= 1'b0;
+		//#(50_000)				start	<= 1'b1;
+		//#(75_000)				start	<= 1'b0;
 
         #PERIOD $finish;
     end
